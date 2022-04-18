43017f3e29ed0828936e4444f6103409f4be396a
kartikdiwan <71651448+kartikdiwan@users.noreply.github.com>
2022-04-18 16:21:48 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, re_path
-from . import views
-urlpatterns = [
-path(' ', views.index, name='index'),
-re_path(r'^(?P<year>[0-9]{4})/(?P<choice>[1-2])/', views.index, name='index')
-]
